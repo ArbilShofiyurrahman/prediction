be2f0c78895baf9a4429d221ac7ef8f9a8d7e643
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2022-12-01 10:16:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,38 @@ with tab1:
    st.write("Belum ada tab deskripsi data")
 
 with tab2:
+   # FUNCTION
+   def user_report():
+      pregnancies = st.sidebar.slider('Pregnancies', 0,17, 3 )
+      glucose = st.sidebar.slider('Glucose', 0,200, 120 )
+      bp = st.sidebar.slider('Blood Pressure', 0,122, 70 )
+      skinthickness = st.sidebar.slider('Skin Thickness', 0,100, 20 )
+      insulin = st.sidebar.slider('Insulin', 0,846, 79 )
+      bmi = st.sidebar.slider('BMI', 0,67, 20 )
+      dpf = st.sidebar.slider('Diabetes Pedigree Function', 0.0,2.4, 0.47 )
+      age = st.sidebar.slider('Age', 21,88, 33 )
+     
+   user_report_data = {
+      'pregnancies':pregnancies,
+      'glucose':glucose,
+      'bp':bp,
+      'skinthickness':skinthickness,
+      'insulin':insulin,
+      'bmi':bmi,
+      'dpf':dpf,
+      'age':age
+   }
+  report_data = pd.DataFrame(user_report_data, index=[0])
+  return report_data
+
+
+  # PATIENT DATA
+   user_data = user_report()
+   st.subheader('Patient Data')
+   st.write(user_data)
+   
    st.write("belum pre processing")
+   
    # Age vs Pregnancies
    st.header('Pregnancy count Graph (Others vs Yours)')
    fig_preg = plt.figure()
