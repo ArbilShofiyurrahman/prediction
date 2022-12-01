df6a089f87d3dc82470a300ac5bf021d18e05cfc
ARBIL SHOFIYURRAHMAN <111713911+ArbilShofiyurrahman@users.noreply.github.com>
2022-12-01 10:19:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,9 +52,8 @@ with tab2:
       'dpf':dpf,
       'age':age
    }
-   
-      report_data = pd.DataFrame(user_report_data, index=[0])
-      return report_data
+   report_data = pd.DataFrame(user_report_data, index=[0])
+   return report_data
 
 
   # PATIENT DATA
